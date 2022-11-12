c26ef7cee764cbede8c2cd7d89f805b54f3b38d6
jorgegoco <jorgegoco70@gmail.com>
2022-11-12 12:34:28 +0100

    Clean repeated code

--- src/checkbox.js
+++
@@ -1,7 +0,0 @@
-export default function deleteChecked(toDOList) {
-  const checkedArray = toDOList.arrayTasks.filter((task) => task.completed === false);
-  for (let i = 0; i < checkedArray.length; i += 1) {
-    checkedArray[i].index = i + 1;
-  }
-  localStorage.setItem('tasksData', JSON.stringify(checkedArray));
-}
