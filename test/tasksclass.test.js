7a177ffd25d2b4e0b8b5950b1ceba7ff942d8525
jorgegoco <jorgegoco70@gmail.com>
2022-11-17 15:33:14 +0100

    Add editing and updating check status

--- test/tasksclass.test.js
+++ test/tasksclass.test.js
@@ -1,6 +1,6 @@
 import Tasks from '../src/modules/tasksclass.js';
 
-describe('Test the "add" and "remove" functions in the "Tasks" class', () => {
+describe('Test the CRUD functions in the "Tasks" class', () => {
   const newToDoList = new Tasks();
 
   test('"addTask" function', () => {
@@ -15,5 +15,21 @@ describe('Test the "add" and "remove" functions in the "Tasks" class', () => {
     expect(newToDoList.arrayTasks.length).toBe(1);
     newToDoList.removeTask(1);
     expect(newToDoList.arrayTasks.length).toBe(0);
+  });
+
+  test('"updateTask" function', () => {
+    newToDoList.addTask('New task');
+    newToDoList.updateTask(1, 'Updated task');
+    expect(newToDoList.arrayTasks[0].description).toBe('Updated task');
+    newToDoList.removeTask(1);
+  });
+
+  test('"updateCheckbox" function', () => {
+    newToDoList.addTask('New task');
+    newToDoList.updateCheckbox(1, true);
+    expect(newToDoList.arrayTasks[0].completed).toBe(true);
+    newToDoList.updateCheckbox(1, false);
+    expect(newToDoList.arrayTasks[0].completed).toBe(false);
+    newToDoList.removeTask(1);
   });
 });
